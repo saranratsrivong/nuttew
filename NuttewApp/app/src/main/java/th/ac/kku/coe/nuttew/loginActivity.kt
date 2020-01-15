38f550781d18fe18f0d8aef3d27f6857b4c71ccf
saranrat srivong <saranratsrivong@github.com>
2020-01-15 17:27:07 +0700

    ver0.1
    first commit !
    project nuttew
    version 0.1
    
    - already create activity page for our code

---
+++ NuttewApp/app/src/main/java/th/ac/kku/coe/nuttew/loginActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.kku.coe.nuttew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ NuttewApp/app/src/main/java/th/ac/kku/coe/nuttew/requestFriendActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.kku.coe.nuttew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class requestFriendActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request_friend)
+    }
+}
