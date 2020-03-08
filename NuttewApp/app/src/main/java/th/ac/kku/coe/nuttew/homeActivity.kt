ab038e0ee3858e948f84f06a479d2074d961c3ea
pornsukon <pornsukon@kkumail.com>
2020-03-08 20:01:08 +0700

    yong
    -settings
    -profile
    -report

--- NuttewApp/app/src/main/java/th/ac/kku/coe/nuttew/homeActivity.kt
+++ NuttewApp/app/src/main/java/th/ac/kku/coe/nuttew/homeActivity.kt
@@ -1,12 +1,39 @@
 package th.ac.kku.coe.nuttew
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class homeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun friendPage(view: View) {
+        val intent = Intent(this,
+            requestFriendActivity::class.java).apply {
+
+        }
+        startActivity(intent);
+    }
+
+    fun chatPage(view: View) {
+        val intent = Intent(this,
+            chatActivity::class.java).apply {
+
+        }
+        startActivity(intent);
+    }
+
+    fun settingsPage(view: View) {
+        val intent = Intent(this,
+            settingActivity::class.java).apply {
+
+        }
+        startActivity(intent);
     }
 }
--- NuttewApp/app/src/main/java/th/ac/kku/coe/nuttew/settingActivity.kt
+++ NuttewApp/app/src/main/java/th/ac/kku/coe/nuttew/settingActivity.kt
@@ -1,12 +1,22 @@
 package th.ac.kku.coe.nuttew
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class settingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+    }
+
+    fun profilePage(view: View) {
+        val intent = Intent(this,
+            profileActivity::class.java).apply {
+
+        }
+        startActivity(intent);
     }
 }
